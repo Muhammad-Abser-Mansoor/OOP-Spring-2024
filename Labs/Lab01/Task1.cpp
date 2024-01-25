8e103d437fc5aa6aad4d0e88313fc9e460991d49
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2024-01-25 14:04:09 +0500

    Update Task1.cpp

--- Labs/Lab01/Task1.cpp
+++ Labs/Lab01/Task1.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int x = 0;
+    cout << "Enter a number ";
+    cin >> x;
+    if (x%2 == 0) {
+        cout << "Even";
+    }
+    else {
+        cout << "Odd";
+    }
+    return 0;
+}
