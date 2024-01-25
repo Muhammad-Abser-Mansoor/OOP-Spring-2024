da8d534e2f884fb778bb127fe7a00b255b391a3b
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2024-01-25 14:14:42 +0500

    Create Task2.cpp

---
+++ Labs/Lab01/Task2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void avg (int x,int y, int z) {
+    cout << "Total is " << (x + y + z)/3 << endl;
+    return;
+}
+
+void tot (int x,int y, int z) {
+    cout << "Average is " << x + y + z << endl;
+    return;
+}
+
+void gg (int x) {
+    if (x >= 90) {
+        cout << "A" << endl;
+    }
+    else if (x >= 80) {
+        cout << "B" << endl;
+    }
+    else if (x >= 70) {
+        cout << "C" << endl;
+    }
+    else if (x >= 60) {
+        cout << "D" << endl;
+    }
+    else {
+        cout << "F" << endl;
+    }
+    return;
+}
+
+int main() {
+    int x,y,z;
+    cout << "Enter English Marks ";
+    cin >> x;
+    gg(x);
+    cout << "Enter Maths Marks ";
+    cin >> y;
+    gg(y);
+    cout << "Enter Science Marks ";
+    cin >> z;
+    gg(z);
+    tot(x,y,z);
+    avg(x,y,z);
+    return 0;
+}
