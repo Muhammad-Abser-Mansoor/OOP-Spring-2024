2409af9bed8a1e4e5844f7cd0ea38aa8421c2221
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2024-02-20 12:41:44 +0500

    Update L4T4.cpp

--- Labs/Lab04/L4T4.cpp
+++ Labs/Lab04/L4T4.cpp
@@ -15,12 +15,22 @@ class Office{
 
     public:
 
-    Office() : Location("") : SeatingCapacity(0) {}
+    Office() : Location("") , SeatingCapacity(0) , furniture({"","",""}) {}
 
     Office(std::string loc) : Location(loc) {}
 
     Office(int cap) : SeatingCapacity(cap) {}
 
-    Office(std::string loc, int cap) : Location(loc) : SeatingCapacity(cap) {}
+    Office(std::string loc, int cap) : Location(loc) , SeatingCapacity(cap) {}
 
-    Office(std::string fur1, std::string fur2, std::string fur3) :
+    Office(std::string fur1, std::string fur2, std::string fur3, std::string loc, int cap) : furniture({fur1,fur2,fur3}) , Location(loc) , SeatingCapacity(cap) {}
+};
+
+int main() {
+    
+    Office office1;
+    Office office2("Karachi");
+    Office office3(50);
+    Office office4("Lahore",100);
+    Office office5("Table","Kursi","Feder","Russia",500);
+}
