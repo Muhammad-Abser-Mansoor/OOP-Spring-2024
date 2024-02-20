e185938cea0529d67c74c154818d14e6d05d495d
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2024-02-20 12:30:59 +0500

    Create L4T2.cpp

---
+++ Labs/Lab04/L4T2.cpp
@@ -0,0 +1,60 @@
+/* Programmer: Muhammad Abser Mansoor
+ * Date: 15/2/2024
+ * Desc.: Book Class
+ */
+
+ #include <iostream>
+ #include <string>
+ #include <vector>
+
+class Book {
+
+    std::string name;
+    std::string author;
+    int ISBN;
+    int totalpages;
+    int numpage;
+
+    public:
+
+    Book() : totalpages(1000), numpage(0) {}
+    
+    void setname(std::string newname) {
+        name = newname;
+    }
+    
+    void setauthor(std::string newauthor) {
+        author = newauthor;
+    }
+    
+    void setISBN(int newISBN) {
+        ISBN = newISBN;
+    }
+
+    void pagesread(int n) {
+
+        numpage = n;
+        if (numpage == totalpages)
+            std::cout << "You have finished the book" << std::endl;
+            else {
+            std::cout << "You have not finished the book" << std::endl; }
+    }
+    
+    void showbookdetails() {
+        std::cout << "Book name is " << name << std::endl;
+        std::cout << "Author's name is " << author << std::endl;
+        std::cout << "Total pages are " << totalpages << std::endl;
+        std::cout << "Number of pages read are " << numpage << std::endl;
+    }
+};
+
+int main() {
+
+    Book bokoen1;
+
+    bokoen1.pagesread(555);
+    bokoen1.setauthor("Abser");
+    bokoen1.setISBN(1111);
+    bokoen1.setname("HOI4 Guide");
+    bokoen1.showbookdetails();
+}
